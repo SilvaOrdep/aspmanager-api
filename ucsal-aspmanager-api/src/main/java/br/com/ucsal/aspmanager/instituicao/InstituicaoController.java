229b2788f4776c9965774d972cf2cf9dd94110c9
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-14 21:58:47 -0300

    feat: Controller de IES criado

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/instituicao/InstituicaoController.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/instituicao/InstituicaoController.java
@@ -1,4 +1,22 @@
 package br.com.ucsal.aspmanager.instituicao;
 
-public class InstituicaoController {
+import br.com.ucsal.aspmanager.shared.controller.AbstractCrudController;
+import br.com.ucsal.aspmanager.shared.service.ServiceBase;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/instituicao")
+public class InstituicaoController extends AbstractCrudController {
+    public InstituicaoController(InstituicaoService instituicaoService) {
+        super(instituicaoService);
+    }
+
+    @Override
+    protected URI location(Object createDTO, UriComponentsBuilder uriBuilder) {
+        return null;
+    }
 }
