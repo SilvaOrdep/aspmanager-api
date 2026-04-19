067601f3ae19c31a07e8aa9fbf65335378e75d5d
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-19 19:23:59 -0300

    feat: IES controller implemented

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/instituicao/controller/InstituicaoController.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/instituicao/controller/InstituicaoController.java
@@ -1,5 +1,8 @@
 package br.com.ucsal.aspmanager.instituicao.controller;
 
+import br.com.ucsal.aspmanager.instituicao.dto.request.CreateInstituicaoEnsinoRequest;
+import br.com.ucsal.aspmanager.instituicao.dto.request.UpdateInstituicaoEnsinoRequest;
+import br.com.ucsal.aspmanager.instituicao.dto.response.InstituicaoEnsinoResponse;
 import br.com.ucsal.aspmanager.instituicao.service.InstituicaoService;
 import br.com.ucsal.aspmanager.shared.controller.AbstractCrudController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +13,17 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/api/instituicao")
-public class InstituicaoController extends AbstractCrudController {
-    public InstituicaoController(InstituicaoService instituicaoService) {
+public class InstituicaoController extends AbstractCrudController<Long,
+        CreateInstituicaoEnsinoRequest, UpdateInstituicaoEnsinoRequest, InstituicaoEnsinoResponse> {
+    private final InstituicaoService instituicaoService;
+
+    public InstituicaoController(InstituicaoService instituicaoService, InstituicaoService instituicaoService1) {
         super(instituicaoService);
+        this.instituicaoService = instituicaoService1;
     }
 
     @Override
-    protected URI location(Object createDTO, UriComponentsBuilder uriBuilder) {
-        return null;
+    protected URI location(InstituicaoEnsinoResponse instituicaoEnsino , UriComponentsBuilder uriBuilder) {
+        return uriBuilder.path("/api/v1/instituicao/{id}").buildAndExpand(instituicaoEnsino.id()).toUri();
     }
 }
