a3145c83385f828ea43ba7edb3baf91de91f90e9
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-20 00:19:37 -0300

    feat: create operations from Escola implemented on Escola Service

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/escola/service/EscolaService.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/escola/service/EscolaService.java
@@ -6,13 +6,25 @@ import br.com.ucsal.aspmanager.escola.dto.request.UpdateDisciplinaRequest;
 import br.com.ucsal.aspmanager.escola.dto.request.UpdateEscolaRequest;
 import br.com.ucsal.aspmanager.escola.dto.response.DisciplinaResponse;
 import br.com.ucsal.aspmanager.escola.dto.response.EscolaResponse;
+import br.com.ucsal.aspmanager.escola.model.Disciplina;
+import br.com.ucsal.aspmanager.escola.model.Escola;
 import br.com.ucsal.aspmanager.escola.repository.DisciplinaRepository;
 import br.com.ucsal.aspmanager.escola.repository.EscolaRepository;
 
+import br.com.ucsal.aspmanager.instituicao.model.InstituicaoEnsino;
+import br.com.ucsal.aspmanager.instituicao.repository.InstituicaoEnsinoRepository;
+import br.com.ucsal.aspmanager.shared.model.enums.StatusRegistro;
 import br.com.ucsal.aspmanager.shared.service.ServiceBase;
+import br.com.ucsal.aspmanager.usuario.model.Professor;
+import br.com.ucsal.aspmanager.usuario.repository.ProfessorRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EscolaService implements ServiceBase<Long,
@@ -20,16 +32,50 @@ public class EscolaService implements ServiceBase<Long,
 
     private final EscolaRepository escolas;
     private final DisciplinaRepository disciplinas;
+    private InstituicaoEnsinoRepository instituicoes;
+    private ProfessorRepository professores;
 
-    public EscolaService(EscolaRepository escolas, DisciplinaRepository disciplinas){
+    public EscolaService(EscolaRepository escolas, DisciplinaRepository disciplinas,
+                         InstituicaoEnsinoRepository instituicoes, ProfessorRepository professores){
         this.escolas = escolas;
         this.disciplinas = disciplinas;
+        this.instituicoes = instituicoes;
+        this.professores = professores;
     }
-
 
     @Override
     public EscolaResponse criar(CreateEscolaRequest createEscolaRequest) {
-        return null;
+
+        InstituicaoEnsino instituicao = instituicoes.findById(createEscolaRequest.idInstituicao()).
+                orElseThrow(() -> new EntityNotFoundException(("Instituição de ensino não encontrada!")));
+
+        Professor professor = professores.findById(createEscolaRequest.idCoordenador()).
+                orElseThrow(() -> new EntityNotFoundException(("Professor não encontrado!")));
+
+        List<Long> idsDisciplinas = createEscolaRequest.idsDisciplinas();
+        List<Disciplina> disciplinas = null;
+
+        if(!idsDisciplinas.isEmpty()){
+
+            for(Long idDisciplina : idsDisciplinas){
+
+                Optional <Disciplina> disciplina = this.disciplinas.findById(idDisciplina);
+                disciplina.ifPresent(value -> disciplinas.add(value));
+
+            }
+        }
+
+        Escola escola = Escola.builder().
+                nome(createEscolaRequest.nome()).
+                instituicao(instituicao).
+                statusRegistro(StatusRegistro.ATIVO).
+                coordenador(professor).
+                disciplinas(disciplinas).
+                build();
+
+        return new EscolaResponse(escola.getId(), escola.getNome(), escola.getStatusRegistro(),
+                escola.getInstituicao().getId(), escola.getCoordenador().getId(),
+                escola.getDisciplinas().stream().map(Disciplina::getId).toList());
     }
 
     @Override
