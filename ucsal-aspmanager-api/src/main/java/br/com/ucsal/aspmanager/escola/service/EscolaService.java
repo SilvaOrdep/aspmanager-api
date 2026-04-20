8d24437156f52dd5d3822d22d96692340983fc2c
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-19 22:20:09 -0300

    feat: Escola Service created

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/escola/service/EscolaService.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/escola/service/EscolaService.java
@@ -1,4 +1,54 @@
 package br.com.ucsal.aspmanager.escola.service;
 
-public class EscolaService {
+import br.com.ucsal.aspmanager.escola.dto.request.CreateEscolaRequest;
+import br.com.ucsal.aspmanager.escola.dto.request.UpdateEscolaRequest;
+import br.com.ucsal.aspmanager.escola.dto.response.EscolaResponse;
+import br.com.ucsal.aspmanager.escola.model.Disciplina;
+import br.com.ucsal.aspmanager.escola.repository.DisciplinaRepository;
+import br.com.ucsal.aspmanager.escola.repository.EscolaRepository;
+import br.com.ucsal.aspmanager.instituicao.dto.request.CreateInstituicaoEnsinoRequest;
+import br.com.ucsal.aspmanager.instituicao.dto.request.UpdateInstituicaoEnsinoRequest;
+import br.com.ucsal.aspmanager.instituicao.dto.response.InstituicaoEnsinoResponse;
+import br.com.ucsal.aspmanager.shared.service.ServiceBase;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EscolaService implements ServiceBase<Long,
+        CreateEscolaRequest, UpdateEscolaRequest, EscolaResponse> {
+
+    private final EscolaRepository escolas;
+    private final DisciplinaRepository disciplinas;
+
+    public EscolaService(EscolaRepository escolas, DisciplinaRepository disciplinas){
+        this.escolas = escolas;
+        this.disciplinas = disciplinas;
+    }
+
+
+    @Override
+    public EscolaResponse criar(CreateEscolaRequest createEscolaRequest) {
+        return null;
+    }
+
+    @Override
+    public Page<EscolaResponse> buscarTodos(Pageable filtros) {
+        return null;
+    }
+
+    @Override
+    public EscolaResponse buscar(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public EscolaResponse atualizar(Long aLong, UpdateEscolaRequest updateEscolaRequest) {
+        return null;
+    }
+
+    @Override
+    public void deletar(Long aLong) {
+
+    }
 }
