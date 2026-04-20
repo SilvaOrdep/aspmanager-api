954e49b68d5898e4205beb780252fe72f0a4cb01
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-20 00:14:28 -0300

    feat: builder added on Disciplina model

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/escola/model/Disciplina.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/escola/model/Disciplina.java
@@ -3,6 +3,7 @@ package br.com.ucsal.aspmanager.escola.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "disciplinas")
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Disciplina {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
