db2a6993f64ff05fc528c3dc9ffa7149aa1945c8
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-19 22:19:52 -0300

    feat: Escola methods added on Escola Controller

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/escola/controller/EscolaController.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/escola/controller/EscolaController.java
@@ -1,4 +1,31 @@
 package br.com.ucsal.aspmanager.escola.controller;
 
-public class EscolaController {
+import br.com.ucsal.aspmanager.escola.dto.request.CreateEscolaRequest;
+import br.com.ucsal.aspmanager.escola.dto.request.UpdateEscolaRequest;
+import br.com.ucsal.aspmanager.escola.dto.response.EscolaResponse;
+import br.com.ucsal.aspmanager.escola.service.EscolaService;
+import br.com.ucsal.aspmanager.shared.controller.AbstractCrudController;
+
+import br.com.ucsal.aspmanager.shared.service.ServiceBase;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/escola")
+public class EscolaController extends AbstractCrudController<Long,
+        CreateEscolaRequest, UpdateEscolaRequest, EscolaResponse> {
+
+    private final EscolaService escolaService;
+
+    public EscolaController(EscolaService escolaService) {
+        super(escolaService);
+        this.escolaService = escolaService;
+    }
+
+    @Override
+    protected URI location(EscolaResponse escola, UriComponentsBuilder uriBuilder) {
+        return uriBuilder.path("/api/v1/escola/{id}").buildAndExpand(escola.id()).toUri();
+    }
 }
