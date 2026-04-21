257468b3d4d34d31d41a97e989a21511debf2e42
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-21 00:46:11 -0300

    chore: swagger config transfered to Application file

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/UcsalAspmanagerApiApplication.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/UcsalAspmanagerApiApplication.java
@@ -1,9 +1,18 @@
 package br.com.ucsal.aspmanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ASP Manager API",
+                version = "1.0.0",
+                description = "Documentação oficial das APIs de gerenciamento escolar da UCSAL."
+        )
+)
 public class UcsalAspmanagerApiApplication {
 
     public static void main(String[] args) {
