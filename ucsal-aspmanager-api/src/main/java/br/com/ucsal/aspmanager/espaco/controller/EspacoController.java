7d0da0730dc488b1ad6b3af5e684dd3628a41607
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-20 22:31:56 -0300

    feat: Solicitacao Post method implemented on Espaco Controller

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/espaco/controller/EspacoController.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/espaco/controller/EspacoController.java
@@ -1,11 +1,17 @@
 package br.com.ucsal.aspmanager.espaco.controller;
 
 import br.com.ucsal.aspmanager.espaco.dto.request.CreateEspacoRequest;
+import br.com.ucsal.aspmanager.espaco.dto.request.CreateSolicitacaoRequest;
 import br.com.ucsal.aspmanager.espaco.dto.request.UpdateEspacoRequest;
 import br.com.ucsal.aspmanager.espaco.dto.response.EspacoResponse;
+import br.com.ucsal.aspmanager.espaco.dto.response.SolicitacaoResponse;
 import br.com.ucsal.aspmanager.espaco.service.EspacoService;
 import br.com.ucsal.aspmanager.shared.controller.AbstractCrudController;
 import br.com.ucsal.aspmanager.shared.service.ServiceBase;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -28,4 +34,18 @@ public class EspacoController extends AbstractCrudController<Long,
     protected URI location(EspacoResponse espaco, UriComponentsBuilder uriBuilder) {
         return uriBuilder.path("/api/v1/espaco/{id}").buildAndExpand(espaco.id()).toUri();
     }
+
+    @PostMapping("/solicitacao")
+    public ResponseEntity<SolicitacaoResponse> criarSolicitacao(@Valid @RequestBody CreateSolicitacaoRequest createSolicitacao, UriComponentsBuilder uriBuilder){
+        SolicitacaoResponse solicitacaoResponse = espacoService.criarSolicitacao(createSolicitacao);
+        URI uri = espacoLocation(solicitacaoResponse, uriBuilder);
+
+        return ResponseEntity.created(uri).body(solicitacaoResponse);
+    }
+
+    protected URI espacoLocation(SolicitacaoResponse solicitacao, UriComponentsBuilder uriBuilder){
+        return uriBuilder.path("/api/v1/espaco/solicitacao{id}").buildAndExpand(solicitacao.idSolicitacao()).toUri();
+    }
+
+
 }
