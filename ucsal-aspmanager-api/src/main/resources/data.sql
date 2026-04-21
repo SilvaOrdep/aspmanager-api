2d6aa1fd6acf7f44757689f66e9e2e4636ada106
Pedro Silva <pedro.o.silva975@gmail.com>
2026-04-21 03:02:19 -0300

    chore: dados iniciais para IES e escola

--- ucsal-aspmanager-api/src/main/resources/data.sql
+++ ucsal-aspmanager-api/src/main/resources/data.sql
@@ -8,4 +8,16 @@ VALUES (
 	'ATIVO'
 );
 
+INSERT INTO instituicoes_ensino (id, nome, endereco)
+VALUES
+    (1, 'UCSAL - Campus Pituaçu', 'Av. Pinto de Aguiar, 2589 - Pituaçu, Salvador - BA');
+
+INSERT INTO escolas (id, nome, status_registro, id_instituicao, id_professor_coordenador)
+VALUES
+    (1, 'Escola de Tecnologia e Sistemas', 'ATIVO', 1, NULL);
+
+ALTER SEQUENCE instituicoes_ensino_id_seq RESTART WITH 2;
+ALTER SEQUENCE escolas_id_seq RESTART WITH 2;
 ALTER SEQUENCE usuarios_id_seq RESTART WITH 2;
+
+
