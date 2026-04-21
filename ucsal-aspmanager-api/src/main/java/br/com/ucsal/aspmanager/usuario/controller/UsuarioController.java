c4bf3bc70050625ed3c032c5e67fb782007c6d24
Pedro Silva <pedro.o.silva975@gmail.com>
2026-04-21 02:05:30 -0300

    feat: rotas adicionais no controller para gerenciamento de professores

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/usuario/controller/UsuarioController.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/usuario/controller/UsuarioController.java
@@ -1,12 +1,16 @@
 package br.com.ucsal.aspmanager.usuario.controller;
 
 import br.com.ucsal.aspmanager.shared.controller.AbstractCrudController;
+import br.com.ucsal.aspmanager.usuario.dto.request.AlterarSenhaRequest;
 import br.com.ucsal.aspmanager.usuario.dto.request.CreateUsuarioRequest;
+import br.com.ucsal.aspmanager.usuario.dto.request.UpdateProfessorRequest;
 import br.com.ucsal.aspmanager.usuario.dto.request.UpdateUsuarioRequest;
 import br.com.ucsal.aspmanager.usuario.dto.response.UsuarioResponse;
 import br.com.ucsal.aspmanager.usuario.service.UsuarioService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
@@ -20,6 +24,33 @@ public class UsuarioController extends AbstractCrudController<Long, CreateUsuari
     public UsuarioController(UsuarioService usuarioService) {
         super(usuarioService);
         this.usuarioService = usuarioService;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> alterarStatus(@PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.alterarStatusRegistro(id));
+    }
+
+    @PatchMapping("/{id}/alterar-senha")
+    public ResponseEntity<Void> alterarSenha(@PathVariable Long id, AlterarSenhaRequest request) {
+        usuarioService.alterarSenha(request, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/professores")
+    public ResponseEntity<Page<UsuarioResponse>> buscarTodosOsProfessores(Pageable filtros) {
+        return ResponseEntity.ok(usuarioService.buscarTodosProfessores(filtros));
+    }
+
+    @PutMapping("/professores/{idProfessor}")
+    public ResponseEntity<UsuarioResponse> atualizarProfessor(@PathVariable Long idProfessor, @RequestBody UpdateProfessorRequest request) {
+        return ResponseEntity.ok(usuarioService.atualizarProfessor(idProfessor, request));
+    }
+
+    @DeleteMapping("/professores/{idProfessor}")
+    public ResponseEntity<Void> deletarProfessor(@PathVariable Long idProfessor) {
+        usuarioService.deletarProfessor(idProfessor);
+        return ResponseEntity.noContent().build();
     }
 
     @Override
