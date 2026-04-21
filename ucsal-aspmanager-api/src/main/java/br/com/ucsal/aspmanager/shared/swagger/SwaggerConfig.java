acb7d475bdf79c670c46875f08a12bb597cdaba9
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-21 00:35:15 -0300

    chore: swagger configuration set

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/shared/swagger/SwaggerConfig.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/shared/swagger/SwaggerConfig.java
@@ -1,4 +1,16 @@
 package br.com.ucsal.aspmanager.shared.swagger;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customizacaoOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ASP Manager API")
+                        .version("1.0.0")
+                        .description("Documentação oficial das APIs de gerenciamento escolar da UCSAL."));
+    }
 }
