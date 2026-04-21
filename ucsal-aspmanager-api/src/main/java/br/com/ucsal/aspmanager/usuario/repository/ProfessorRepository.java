6e77dcba807774a679965d6457b6f1a55503cbd8
Pedro Silva <pedro.o.silva975@gmail.com>
2026-04-21 01:48:14 -0300

    feat: métodos de apoio para busca de entidades com base no statusRegistro do usuário

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/usuario/repository/ProfessorRepository.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/usuario/repository/ProfessorRepository.java
@@ -1,10 +1,19 @@
 package br.com.ucsal.aspmanager.usuario.repository;
 
+import br.com.ucsal.aspmanager.shared.model.enums.StatusRegistro;
 import br.com.ucsal.aspmanager.usuario.model.Professor;
+import br.com.ucsal.aspmanager.usuario.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 	Optional<Professor> findByUsuario_Id(Long usuarioId);
+    Page<Professor> findByUsuario_StatusRegistro(StatusRegistro statusRegistro, Pageable pageable);
+
+    void deleteByUsuario(Usuario usuario);
+
+    Optional<Professor> findByMatricula(String matricula);
 }
--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/usuario/repository/UsuarioRepository.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/usuario/repository/UsuarioRepository.java
@@ -1,11 +1,15 @@
 package br.com.ucsal.aspmanager.usuario.repository;
 
+import br.com.ucsal.aspmanager.shared.model.enums.StatusRegistro;
 import br.com.ucsal.aspmanager.usuario.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findUsuarioByEmail(String email);
+    Page<Usuario> findByStatusRegistro(StatusRegistro statusRegistro, Pageable pageable);
 
 }
