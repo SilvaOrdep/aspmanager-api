a025e3c64b6dcfee45818869172e3e456e3b9cc8
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-20 22:06:02 -0300

    feat: Solicitacao Espaco DTOs implemented

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/espaco/dto/request/CreateSolicitacaoRequest.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/espaco/dto/request/CreateSolicitacaoRequest.java
@@ -1,6 +1,24 @@
 package br.com.ucsal.aspmanager.espaco.dto.request;
 
-public record CreateSolicitacaoRequest(
+import br.com.ucsal.aspmanager.espaco.model.Espaco;
+import br.com.ucsal.aspmanager.shared.model.enums.StatusSolicitacao;
+import br.com.ucsal.aspmanager.usuario.model.Professor;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record CreateSolicitacaoRequest(
+        @NotNull(message = "Data não pode ser nula!")
+        LocalDate dataUso,
+        @NotNull(message = "Hora início não pode ser nula!")
+        LocalTime horaInicio,
+        @NotNull(message = "Hora fim não pode ser nula!")
+        LocalTime horaFim,
+        @NotNull(message = "Id do espaço não pode ser nulo!")
+        Long idEspaco,
+        @NotNull(message = "Id do professor não pode ser nulo!")
+        Long idProfessor
 ) {
 }
--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/espaco/dto/response/SolicitacaoResponse.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/espaco/dto/response/SolicitacaoResponse.java
@@ -1,6 +1,18 @@
 package br.com.ucsal.aspmanager.espaco.dto.response;
 
+import br.com.ucsal.aspmanager.shared.model.enums.StatusSolicitacao;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public record SolicitacaoResponse(
-        
+        Long idSolicitacao,
+        LocalDate dataUso,
+        LocalTime horaInicio,
+        LocalTime horaFim,
+        Long idEspaco,
+        Long idProfessor,
+        StatusSolicitacao statusSolicitacao
 ) {
 }
