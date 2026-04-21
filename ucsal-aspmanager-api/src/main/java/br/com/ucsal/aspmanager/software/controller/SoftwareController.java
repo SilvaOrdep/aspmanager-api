fea1a177ff436f007464b69d120fb8a4ed8e87f5
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-21 16:56:48 -0300

    feat: Software Controller implemented

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/software/controller/SoftwareController.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/software/controller/SoftwareController.java
@@ -1,4 +1,32 @@
 package br.com.ucsal.aspmanager.software.controller;
 
-public class SoftwareController {
+import br.com.ucsal.aspmanager.shared.controller.AbstractCrudController;
+import br.com.ucsal.aspmanager.shared.service.ServiceBase;
+import br.com.ucsal.aspmanager.software.dto.request.CreateSoftwareRequest;
+import br.com.ucsal.aspmanager.software.dto.request.UpdateSoftwareRequest;
+import br.com.ucsal.aspmanager.software.dto.response.SoftwareResponse;
+import br.com.ucsal.aspmanager.software.model.Software;
+import br.com.ucsal.aspmanager.software.service.SoftwareService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/software")
+public class SoftwareController extends AbstractCrudController<Long,
+        CreateSoftwareRequest, UpdateSoftwareRequest, SoftwareResponse> {
+
+    private final SoftwareService softwareService;
+
+    public SoftwareController(SoftwareService softwareService) {
+        super(softwareService);
+        this.softwareService = softwareService;
+    }
+
+    @Override
+    protected URI location(SoftwareResponse software, UriComponentsBuilder uriBuilder) {
+        return uriBuilder.path("/api/v1/software/{id}").buildAndExpand(software.id()).toUri();
+    }
 }
