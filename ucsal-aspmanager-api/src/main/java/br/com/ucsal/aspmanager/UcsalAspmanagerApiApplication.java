a82effaf8639a2b77e62854f162208bc76cc5407
GabrielFraga <gabrielbff2006@gmail.com>
2026-04-21 13:25:11 -0300

    refactor: jwt token added on swagger

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/UcsalAspmanagerApiApplication.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/UcsalAspmanagerApiApplication.java
@@ -1,7 +1,10 @@
 package br.com.ucsal.aspmanager;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +15,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
                 version = "1.0.0",
                 description = "Documentação oficial das APIs de gerenciamento escolar da UCSAL."
         )
+)
+
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "Insira o token JWT gerado no endpoint de login",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class UcsalAspmanagerApiApplication {
 
