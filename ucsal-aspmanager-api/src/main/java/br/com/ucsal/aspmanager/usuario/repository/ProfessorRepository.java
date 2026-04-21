acc52a4222ce07460f341dbaea60529d122a49a1
Pedro Silva <pedro.o.silva975@gmail.com>
2026-04-21 00:30:30 -0300

    refactor: ajuste no repository de usuário e professor

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/usuario/repository/ProfessorRepository.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/usuario/repository/ProfessorRepository.java
@@ -3,5 +3,8 @@ package br.com.ucsal.aspmanager.usuario.repository;
 import br.com.ucsal.aspmanager.usuario.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+	Optional<Professor> findByUsuario_Id(Long usuarioId);
 }
