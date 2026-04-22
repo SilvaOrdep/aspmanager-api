53dde36e48de06f3276acc98de141bb17f02befe
Pedro Silva <pedro.o.silva975@gmail.com>
2026-04-22 00:10:35 -0300

    feat: métodos auxiliares de busca para software e solicitacaoSoftware

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/software/repository/SoftwareRepository.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/software/repository/SoftwareRepository.java
@@ -3,5 +3,8 @@ package br.com.ucsal.aspmanager.software.repository;
 import br.com.ucsal.aspmanager.software.model.Software;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SoftwareRepository extends JpaRepository<Software, Long> {
+	Optional<Software> findByNomeIgnoreCaseAndVersaoIgnoreCase(String nome, String versao);
 }
--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/software/repository/SolicitacaoSoftwareRepository.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/software/repository/SolicitacaoSoftwareRepository.java
@@ -1,7 +1,10 @@
 package br.com.ucsal.aspmanager.software.repository;
 
 import br.com.ucsal.aspmanager.software.model.SolicitacaoSoftware;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SolicitacaoSoftwareRepository extends JpaRepository<SolicitacaoSoftware, Long> {
+	Page<SolicitacaoSoftware> findByProfessor_Id(Long idProfessor, Pageable pageable);
 }
