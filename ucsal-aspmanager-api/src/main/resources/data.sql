ca5e927008df95fa0755c2c94e5eafe750d4218f
silvaOrdep <pedro.o.silva975@gmail.com>
2026-04-22 14:45:35 -0300

    chore: dados iniciais de escolas e professor

--- ucsal-aspmanager-api/src/main/resources/data.sql
+++ ucsal-aspmanager-api/src/main/resources/data.sql
@@ -14,10 +14,31 @@ VALUES
 
 INSERT INTO escolas (id, nome, status_registro, id_instituicao, id_professor_coordenador)
 VALUES
-    (1, 'Escola de Tecnologia e Sistemas', 'ATIVO', 1, NULL);
+    (1, 'Escola de Educação, Cultura e Humanidades', 'ATIVO', 1, NULL),
+    (2, 'Escola de Ciências Sociais e Aplicadas', 'ATIVO', 1, NULL),
+    (3, 'Escola de Engenharias e Ciências Tecnológicas', 'ATIVO', 1, NULL),
+    (4, 'Escola de Ciências Naturais e da Saúde', 'ATIVO', 1, NULL);
+
+
+INSERT INTO usuarios (id, nome_completo, email, senha, perfil, status_registro)
+VALUES (
+	2,
+	'OsvaldoRequiao Mello',
+	'osvaldo.requiao@ucsal.com.br',
+	'$2a$12$fS4l.WRsZSRHHPqrWSpfduo5dbByLjMhfxrvxowMVNqT2gVpl30Te',
+	'PROFESSOR',
+	'ATIVO'
+);
+
+INSERT INTO professores (id, matricula, id_usuario, id_escola)
+VALUES
+    (1, 200033111, 2, 3);
+
+UPDATE escolas
+SET id_professor_coordenador = 1
+WHERE id = 3;
 
 ALTER SEQUENCE instituicoes_ensino_id_seq RESTART WITH 2;
-ALTER SEQUENCE escolas_id_seq RESTART WITH 2;
-ALTER SEQUENCE usuarios_id_seq RESTART WITH 2;
-
-
+ALTER SEQUENCE escolas_id_seq RESTART WITH 5;
+ALTER SEQUENCE usuarios_id_seq RESTART WITH 3;
+ALTER SEQUENCE professores_id_seq RESTART WITH 2;
