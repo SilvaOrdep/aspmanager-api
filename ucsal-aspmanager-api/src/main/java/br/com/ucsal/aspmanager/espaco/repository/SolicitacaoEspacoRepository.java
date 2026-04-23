7e5d9439c86de99b8db5f7397427d67435e2cd67
Pedro Silva <pedro.o.silva975@gmail.com>
2026-04-22 23:42:51 -0300

    feature: métodos auxiliares para verificação de existencia da entidade alvo nos repositories

--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/espaco/repository/SolicitacaoEspacoRepository.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/espaco/repository/SolicitacaoEspacoRepository.java
@@ -4,4 +4,5 @@ import br.com.ucsal.aspmanager.espaco.model.SolicitacaoEspaco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SolicitacaoEspacoRepository extends JpaRepository<SolicitacaoEspaco, Long> {
+	boolean existsByEspaco_Id(Long espacoId);
 }
--- ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/software/repository/SoftwareRepository.java
+++ ucsal-aspmanager-api/src/main/java/br/com/ucsal/aspmanager/software/repository/SoftwareRepository.java
@@ -2,9 +2,14 @@ package br.com.ucsal.aspmanager.software.repository;
 
 import br.com.ucsal.aspmanager.software.model.Software;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface SoftwareRepository extends JpaRepository<Software, Long> {
 	Optional<Software> findByNomeIgnoreCaseAndVersaoIgnoreCase(String nome, String versao);
+
+	@Query(value = "select count(*) > 0 from espaco_softwares where id_software = :softwareId", nativeQuery = true)
+	boolean existsVinculoEmEspacos(@Param("softwareId") Long softwareId);
 }
